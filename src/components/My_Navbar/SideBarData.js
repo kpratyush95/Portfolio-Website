3df8414a757f0defbf7e2f5e31bf43b637c7d35d
kpratyush95@gmail.com <kpratyush95@gmail.com>
2021-04-02 14:16:32 -0400

    commit after going live

--- src/components/My_Navbar/SideBarData.js
+++ src/components/My_Navbar/SideBarData.js
@@ -6,19 +6,19 @@ import *as IoIcons from "react-icons/io";
 export const SideBarData=[
 {   
     title: 'Home',
-    path: 'Portfolio-website/',
+    path: '/Portfolio-website/',
     icon:<AiIcons.AiFillHome/>,
     cName:'nav-text'
 },
 {   
     title: 'About Me',
-    path: 'Portfolio-website/#about',
+    path: '/Portfolio-website/#about',
     icon:<IoIcons.IoIosPaper/>,
     cName:'nav-text'
 },
 {   
     title: 'Projects',
-    path: 'Portfolio-website/#projects',
+    path: '/Portfolio-website/#projects',
     icon:<AiIcons.AiFillProject/>,
     cName:'nav-text'
 }
