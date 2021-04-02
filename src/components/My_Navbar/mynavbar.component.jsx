dc74e4dcca8fa32405221ef579fa6a35fec47568
kpratyush95@gmail.com <kpratyush95@gmail.com>
2021-04-02 14:18:43 -0400

    commit after going live

--- src/components/My_Navbar/mynavbar.component.jsx
+++ src/components/My_Navbar/mynavbar.component.jsx
@@ -53,7 +53,7 @@ const Mynavbar = () =>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="ml-auto">
-      <Nav.Link href="">Home</Nav.Link>
+      <Nav.Link href="#home">Home</Nav.Link>
       <Nav.Link href="#about">About Me</Nav.Link>
       <Nav.Link href="#projects">Projects</Nav.Link>
     </Nav>
